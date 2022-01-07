85fd43cb000bdcdd470fe54b2abe84fa934f1371
bekturb <bekkgboy2@gmail.com>
2022-01-07 15:13:27 +0600

    initial

---
+++ script.js
@@ -0,0 +1,258 @@
+// peremennye
+
+// var
+// const
+// let
+
+
+// var box = 'apple'
+// console.log(box)
+//
+// let num = 22
+// num = 30
+// console.log(num)
+//
+// const course = 'JavaScript'
+// course = 'Python'
+// console.log(course)
+
+// PEREMENNYILARGA TUURA AT BERUU
+
+// let num;
+// let num12;
+// let numString;
+// let numStringSub;
+// let Num;
+// let _num;
+// let num_;
+// let $num;
+
+// PEREMENNYILARGA TUURA emes bolgon ATtar
+
+// let 12num;
+// let num-12;
+// let NUm118yejb;
+// let --jvnj;
+// let =dvnd;
+// let let;
+// let const;
+
+// let num = 22;
+// let sub = 23;
+// // console.log(num + sub)
+// // console.log(num - sub)
+// // console.log(num * sub)
+// // console.log(num / sub)
+// // console.log(num % sub)
+//
+// alert(num ** sub)
+// confirm(num ** sub)
+// prompt(num ** sub)
+
+// let user = 'Vasya';
+//
+// let age = 36;
+//
+// console.log(user, age)
+
+// const userName = 'Vasya';
+// const user = 'Vasya';
+// console.log(user)
+
+
+// console.log('35' + - "22");
+//
+// console.log("35" * "22");
+
+// error code
+// console.log("558" > 22++);
+//
+// let usersCounter = 0;
+// let newUsers = usersCounter++;
+// console.log(newUsers)
+//
+// console.log( !false && 11 || 18 && !'');
+
+// console.log(18 && false)
+
+
+// let name = 0;
+// console.log(name ?? 'Без имени');
+
+// let resultOne = "80" * 10;
+// console.log(resultOne);
+// console.log(typeof resultOne);
+//
+// let resultTwo = "25" - 5;
+// console.log(resultTwo);
+// console.log(typeof resultTwo);
+//
+//
+// let resultThree = 3 * "javaScript";
+// console.log(resultThree);
+// console.log(typeof resultThree);
+
+// pravilnye
+// console.log('uchim')
+// console.log('JS')
+// console.log('uchim JS');
+// ['uchim', 'JS'].forEach(alert);
+// let str = js;
+// let name = 'Vasya';
+// let myAge = 36;
+// console.log(name)
+
+
+
+// nepravilnye
+// console.log('uchim')console.log('JS')
+// console.log('uchim JS')
+// ['uchim', 'JS'].forEach(alert)
+// let str=js;
+// let name = 'Vasya' let myAge = 36
+
+
+// example of ape rant
+// console.log('js' + ' Study')
+
+// let num = 4;
+// console.log(num);
+// 'js' + ' Study' - vyrajenie bez prisvoeniya
+//
+// let num = 'Uchim JS';
+// console.log(num)
+
+// let userAge = 23;
+// let userAgeInfo = `Bektursun: ${userAge}`;
+// console.log(userAgeInfo);
+//
+// let userHeight = 145 / 0;
+// console.log(userHeight);
+//
+// let user = "145" / "0";
+// console.log(typeof user);
+//
+// let userName;
+// console.log(typeof userName)
+
+// let userAge = 23;
+// let userAgeInfo = "Bektursun: ${userAge}";
+// console.log(userAgeInfo);
+
+
+// console.log('35' * "22");
+//
+// console.log('558' > '22++');
+//
+// let usersCounter = 0;
+// let newUsers = usersCounter++;
+// console.log(newUsers);
+
+// // Math.pow()
+// console.log(Math.pow(2, 4))
+// // Math.floor()
+// let n = 10.1
+// console.log(Math.floor(n))
+// // Math.ceil()
+// console.log(math.ceil(n))
+// // Math.round()
+// console.log(Math.round(n))
+// // Math.trunc()
+// console.log(Math.trunc(n))
+// // Math.random()
+// console.log(Math.random()*100)
+//
+// console.log(Math.floor(Math.random()*100))
+
+// let user = '552' > 22;
+// console.log(user)
+
+// let text = 'stroka';
+// alert(typeof text);
+// console.log(text.length);
+
+// const str = " \n\t abra cadabra \n\t  ";
+// // console.log(str);
+// // const trimStr = str.trim()
+// // console.log(trimStr) //отобразит 'abra cadabra'
+// const trimStr = 'abra cadabra';
+// console.log(trimStr.lastIndexOf(' '));
+//
+// console.log("abcd".localeCompare('bbcd'))  //отобразит -1
+// console.log("dbcd".localeCompare('bbcd'))  //отобразит 1
+// console.log("bbcd".localeCompare('bbcd'))  //отобразит 0
+
+// const str = 'AbrA'
+// const lowCase = str.toLowerCase()
+// const upCase = str.toUpperCase()
+//
+// // console.log(lowCase) //отобразит 'abra'
+// console.log(upCase) //отобразит 'ABRA'
+
+// const str = " \t  Mенин атым Бектурсун \n Mен азыркы убакта javascript'ти уйронуп жатам  "
+// console.log(str);
+// let slice2Str = str.slice(str.indexOf(' ') + 1);
+
+// let user = new object {}; sintaksis konstructor objecta
+
+
+// let student = {
+//     name: 'joomart',
+//     age: 20,
+//     'javascript': 1234,
+// }
+// console.log(student["javascript"])
+// console.log(student.name)
+// console.log(student.age)
+
+// HOMETASK
+// let obj = {
+//     a: 1,
+//     b: 2,
+//     c:3
+// }
+// console.log(obj);
+
+//
+// let obj = {
+//     a: 1,
+//     b: 2,
+//     c:3
+// };
+//
+// alert(obj['c']);
+// alert(obj.c);
+//
+// let obj = {
+//     Bektursun: '1000',
+//     Taalaibek: '500',
+//     Kamil:'200'
+// }
+// alert(obj.Bektursun);
+// alert(obj.Kamil);
+//
+// let obj = {
+//     Bektursun: '1000',
+//     Taalaibek: '500',
+//     Kamil:'200'
+// }
+//
+// // obj.Kamil = 500;
+//
+// alert(obj.Kamil)
+//
+// let obj = {
+//     Bektursun: '1000',
+//     Taalaibek: '500',
+//     Kamil:'200'
+// }
+// obj.Ruslan = '1500';
+//
+// console.log(obj)
+//
+// let obj = {
+//     Bektursun: '1000',
+//     Taalaibek: '500',
+//     Kamil: {salary:'500'}
+// }
+// console.log(obj.Kamil.salary)
